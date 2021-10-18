23f41c38c1e86026c0796d7c962f97121ffb013b
Renato Marques <renatomarquesteles@gmail.com>
2021-10-18 16:50:07 -0300

    Adicionado fontes customizadas

--- App.tsx
+++ App.tsx
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
+import { Kanit_400Regular, Kanit_600SemiBold } from '@expo-google-fonts/kanit';
+
+import { SelectUser } from './src/screens/SelectUser';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Kanit_400Regular, Kanit_600SemiBold });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <SelectUser />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/styles/theme.ts
@@ -0,0 +1,18 @@
+export const theme = {
+  colors: {
+    primary: '#6D3AD3',
+    primaryDark: '#270D5B',
+    primaryLight: '#7A6EAA',
+    secondary: '#B353FF',
+    text: '#250C59',
+    secondaryText: '#BDC2C4',
+    success: '#31D0AA',
+    danger: '#ED4B9E',
+    background: '#FAF9FA',
+    white: '#FFFFFF',
+  },
+  fonts: {
+    text400: 'Kanit_400Regular',
+    text600: 'Kanit_600SemiBold',
+  },
+};
