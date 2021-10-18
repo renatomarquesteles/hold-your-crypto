eb72232c66f8421337907789fcce70d0c421be64
Renato Marques <renatomarquesteles@gmail.com>
2021-10-18 14:35:38 -0300

    first commit

---
+++ src/screens/SelectUser/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+interface Props {}
+
+export const SelectUser = (props: Props) => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
